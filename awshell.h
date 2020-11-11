0f2ac370f98a400be8bcea5e3298dd2c83fe3bb5
Wiston Venera Macias <2170@holbertonschool.com>
2020-11-11 22:07:40 +0000

    Library

---
+++ awshell.h
@@ -0,0 +1,10 @@
+#ifndef AWSHELL_H
+#define AWSHELL_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strdup(char *str);
+char **divide_string(char *str);
+#endif /* AWSHELL */
