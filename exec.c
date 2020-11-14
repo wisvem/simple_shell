72fdff35c15340320aa27ed0fba6ec37958ab60b
Wiston Venera Macías <wisvem@hotmail.com>
2020-11-14 22:37:22 +0000

    Updated to work alone

--- exec.c
+++ exec.c
@@ -1,32 +1,37 @@
 #include "awshell.h"
 
-int main(void)
+/**
+* exec - executes applications
+* @argv: arguments to execute
+* Return: 0 on success
+*/
+int exec(char **argv)
 {
-	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
 	pid_t child_pid;
-	int child_status, i;
-	pid_t dad_pid;
+	int child_status, i = 0;
 
-	for (i = 0; i < 5; i++)
+	child_pid = fork();
+	if (child_pid == 0)
 	{
-		child_pid = fork();
-		if (child_pid == 0)
+		if ((execve(argv[0], argv, NULL) == -1) && argv[0] != NULL)
 		{
-			execve(argv[0], argv, NULL);
-			exit(EXIT_FAILURE);
+			printf("%s: command not found\n", argv[0]);
 		}
-		else if (child_pid < 0)
-		{
-			perror("Falied to fork\n");
-			return(-1);
-		}
-		else
-		{
-			wait(&child_status);
-		}
-		dad_pid = getppid();
-		printf("Padre: %u e hijo: %u\n", dad_pid, child_pid);
+		exit(EXIT_FAILURE);
 	}
-	printf("%s\n", argv[0]);
+	else if (child_pid < 0)
+	{
+		perror("Falied to fork\n");
+		return (-1);
+	}
+	else
+	{
+		wait(&child_status);
+	}
+	while (argv[i])
+	{
+		free(argv[i]), i++;
+	}
+	free(argv);
 	return (0);
 }
