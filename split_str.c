cc28a9657baec1c614350f15226f9ce32b232838
Wiston Venera Macías <wisvem@hotmail.com>
2020-11-14 22:29:36 +0000

    Changed remove_string.c to split_str.c - Added char delim argument

---
+++ split_str.c
@@ -0,0 +1,37 @@
+#include "awshell.h"
+
+/**
+ * split_str - separates strings into words
+ * @str: string to divide
+ * @delim: delimiter
+ * Return: pointers to each word on the array
+ */
+
+char **split_str(char *str, char delim)
+{
+	char *token, *token2, *str2;
+	int words = 0, i = 0;
+	char **wordarray;
+
+	str2 = _strdup(str);
+	token = strtok(str, delim);
+
+	while (token)
+	{
+		token = strtok(NULL, delim);
+		words++;
+	}
+
+	token2 = strtok(str2, delim);
+
+	wordarray = malloc(sizeof(char *) * (words + 1));
+	while (token2 != NULL)
+	{
+		wordarray[i] = _strdup(token2);
+		token2 = strtok(NULL, delim);
+		i++;
+	}
+	wordarray[i] = NULL;
+	free(str2);
+	return (wordarray);
+}
