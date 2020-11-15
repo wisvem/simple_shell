eed83027b00bda6941e021b3d0e019bee1f2297c
Wiston Venera Macías <wisvem@hotmail.com>
2020-11-15 01:01:16 +0000

    Task 3 about PATH donde

--- get_env.c
+++ get_env.c
@@ -1,11 +1,16 @@
 #include "awshell.h"
 
+/**
+* get_env - get the environment
+* @str: string where the enviroment is
+* Return: 0 on succces
+**/
 int get_env(char *av)
 {
-	char *name = _strdup(av); /*CHange function*/
-	int i = 0;
+	char *name = _strdup(av);
+	int i = 0, j = 0;
 	extern char **environ;
-	char **temp = NULL;
+	char **temp = NULL, **temp2 = NULL;
 	(void)av;
 
 	while (environ[i] != NULL)
@@ -13,7 +18,19 @@ int get_env(char *av)
 		temp = split_str(environ[i], "=");
 		if (strcmp(temp[0], name) == 0)
 		{
-			printf("%s : %s", temp[0], temp[1]);
+			temp2 = split_str(temp[1], ":");
+			while (temp2[j] != NULL)
+			{
+				printf("%s\n", temp2[j]);
+				j++;
+			}
+/*
+* 			while (temp[j] != NULL)
+*			{
+*				printf("%s\n", temp[j]);
+*				j++;
+*			}
+*/
 		}
 		i++;
 	}
