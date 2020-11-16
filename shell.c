1bbaf716e1011395aa920102fd270fc01e862faf
Wiston Venera Macías <wisvem@hotmail.com>
2020-11-16 14:49:06 +0000

    Interactive a non interactive mode implemented

--- shell.c
+++ shell.c
@@ -7,21 +7,29 @@
  * @argv: arguments
  * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(void)
 {
 	char *buff = NULL;
 	size_t len = 0;
 	int x = 0;
-	(void)argc, (void)argv;
 
-	write(STDOUT, "aw$: ", 5);
+	if (isatty(STDIN) != 0)
+	{
+		write(STDOUT, "aw$: ", 5);
+	}
 	while ((x = getline(&buff, &len, stdin)) != -1)
 	{
 		buff[x - 1] = '\0';
 		exec(split_str(buff, " "));
-		write(STDOUT, "aw$: ", 5);
+		if (isatty(STDIN) != 0)
+		{
+			write(STDOUT, "aw$: ", 5);
+		}
 	}
-	write(STDOUT, "\n", 1);
+	if (isatty(STDIN) != 0)
+	{
+		write(STDOUT, "\n", 1);
+	}
 	free(buff);
 	return (0);
 }
