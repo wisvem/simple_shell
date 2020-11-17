64caad0c965993f185361353dd15c5d59f4ea74b
Andy Lopez <profesor.de.ingles@aol.com>
2020-11-17 20:17:24 +0000

    Fixed style and updated conditions

--- add_list.c
+++ add_list.c
@@ -5,20 +5,31 @@
 * @head: head of the list
 * @str: element to put in the new node
 * Return: the address of the new element, or NULL if it failed
-**/
+*     /\             | |
+*    /  \   _ __   __| |_   _
+*   / /\ \ | '_ \ / _` | | | |
+*  / ____ \| | | | (_| | |_| |
+* /_/    \_\_|_|_|\__, |\__, | _   _
+* \ \        / (_)   | | __/ || \ | |
+*  \ \  /\  / / _ ___| ||___/ |  \| |
+*   \ \/  \/ / | / __| __/ _ \| . ` |
+*    \  /\  /  | \__ \ || (_) | |\  |
+*     \/  \/   |_|___/\__\___/|_| \_| |- Nov 2020 -|
+*
+*/
 p_list *add_list(p_list **head, const char *str)
 {
 	p_list *newnode = NULL;
 	p_list *lastnode = *head;
 
 	newnode = malloc(sizeof(p_list));
-	if (newnode == NULL)
+	if (!newnode)
 	{
 		return (NULL);
 	}
 	(*newnode).str = strdup(str);
 	(*newnode).next = NULL;
-	if (*head == NULL)
+	if (!*head)
 	{
 		*head = newnode;
 	}
