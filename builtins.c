f8e00ce556a3df34fb9f6be2065cb3781c89e8f0
Andy Lopez <profesor.de.ingles@aol.com>
2020-11-17 21:05:20 +0000

    Fixed style

--- builtins.c
+++ builtins.c
@@ -3,6 +3,18 @@
  * get_builtins - checks if the input command is a builtin
  * @av: variables
  * Return: pointer to the function or NULL if it fails
+ *                     _
+ *     /\             | |
+ *    /  \   _ __   __| |_   _
+ *   / /\ \ | '_ \ / _` | | | |
+ *  / ____ \| | | | (_| | |_| |
+ * /_/    \_\_|_|_|\__, |\__, | _   _
+ * \ \        / (_)   | | __/ || \ | |
+ *  \ \  /\  / / _ ___| ||___/ |  \| |
+ *   \ \/  \/ / | / __| __/ _ \| . ` |
+ *    \  /\  /  | \__ \ || (_) | |\  |
+ *     \/  \/   |_|___/\__\___/|_| \_| |- Nov 2020 -|
+ *
  */
 void (*get_builtins(built_t *av))
 {
@@ -19,7 +31,7 @@ void (*get_builtins(built_t *av))
 	for (i = 0; get[i].function; i++)
 	{
 		if (_strcmp(builtins.av[0], get[i].name) == 0)
-			return(builtins[i].function);
+			return (builtins[i].function);
 	}
 	return (NULL);
 }
