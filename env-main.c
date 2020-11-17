f608184c6e21bae7295380e3384bb174bb04d2d8
Andy Lopez <profesor.de.ingles@aol.com>
2020-11-17 19:28:05 +0000

    _env function changed from main

--- env-main.c
+++ env-main.c
@@ -1,29 +1,22 @@
 #include "awshell.h"
 
 /**
- * main - prints the environment
+ * _env - prints the environment
  *
  * Return: Always 0.
  */
-int main(int ac, char **av, char **env)
+int _env(int ac, char **av, char **env)
 {
 	extern char **environ;
 	unsigned int i;
 	(void)ac, (void)av;
 
 	i = 0;
-	while (env[i] != NULL)
+	while (env[i])
 	{
 		printf("%s\n", env[i]);
 		i++;
 	}
 	i = 0;
-	printf("\n--------------\n");
-	while (environ[i] != NULL)
-	{
-		printf("---\n%s\n---\n", environ[i]);
-		i++;
-	}
-	printf("LS_COLORS : %s\n", getenv("LS_COLORS"));
 	return (0);
 }
