bacae18445652858e3ab4be3d5d151bc4e2fe534
Andy Lopez <profesor.de.ingles@aol.com>
2020-11-19 21:03:08 +0000

    Added function that copies memory area

---
+++ _memcpy.c
@@ -0,0 +1,22 @@
+# include "awshell.h"
+
+/**
+ * _memcpy - funtion that copies memory area.
+ * @dest: string destin
+ * @src: string src
+ * @n: number
+ * Return: pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
