03744210c4f573f5fec4cecf8739649bf29bd6cf
Wiston Venera Macías <wisvem@hotmail.com>
2020-11-20 18:51:24 +0000

    Shell tsting valgrind

--- shell.c
+++ shell.c
@@ -20,8 +20,9 @@
 int main(int ac, char *av[])
 {
 	char *buff = NULL, *buff_w = NULL, **buff_split = NULL;
+	char *command;
 	size_t len = 0;
-	int x = 0;
+	int x = 0, i = 0;
 
 	(void)ac, (void)av;
 	if (isatty(STDIN) != 0)
@@ -32,13 +33,24 @@ int main(int ac, char *av[])
 	while ((x = getline(&buff, &len, stdin)) != -1)
 	{
 		buff[x - 1] = '\0';
-		buff_split = _strdup(buff);
+		buff_split = split_str(buff, " ");
+		buff_w = _strdup(buff_split[0]);
 		
-		if (_strcmp(_which(buff_w), buff_which) == 0)
-
+		command = _strdup(_which(buff_w));
+		if (_strcmp(command, buff_w) != 0)
+		{
+			buff_split[0] = NULL;
+			buff_split[0] = malloc(sizeof(char) * (_strlen(command) + 1));
+			if (buff_split[0])
+			{
+				buff_split[0] = _strdup(command);
+			}
+			i = 0;
+			
+		}
 		if (get_builtins(buff) != 0)
 		{
-			exec(split_str(buff, " "));
+			exec(buff_split);
 		}
 		if (isatty(STDIN) != 0)
 		{
@@ -50,5 +62,7 @@ int main(int ac, char *av[])
 		write(STDOUT, "\n", 1);
 	}
 	free(buff);
+	free(buff_w);
+	free(buff_split);
 	return (0);
 }
