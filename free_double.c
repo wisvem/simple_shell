d2d7b6c5079487d5e9547716c441ba4dfa4f46f8
Andy Lopez <profesor.de.ingles@aol.com>
2020-11-21 16:28:24 +0000

    Function that frees double pointers

---
+++ free_double.c
@@ -0,0 +1,26 @@
+#include "awshell.h"
+
+/**
+ * free_double - frees a double pointer
+ * @argv: pointer to free.
+ * Return: Always 0
+ *                     _
+ *     /\             | |
+ *    /  \   _ __   __| |_   _
+ *   / /\ \ | '_ \ / _` | | | |
+ *  / ____ \| | | | (_| | |_| |
+ * /_/    \_\_|_|_|\__, |\__, | _   _
+ * \ \        / (_)   | | __/ || \ | |
+ *  \ \  /\  / / _ ___| ||___/ |  \| |
+ *   \ \/  \/ / | / __| __/ _ \| . ` |
+ *    \  /\  /  | \__ \ || (_) | |\  |
+ *     \/  \/   |_|___/\__\___/|_| \_| |- Nov 2020 -|
+ */
+void free_double(char **ptr)
+{
+	int i;
+
+	for(i = 0; ptr[i]; i++)
+		free(ptr[i]);
+	free(ptr);
+}
